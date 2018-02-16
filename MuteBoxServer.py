0f79e0bb8c6a759675c15cf6a9ca634fddd3a6e8
rbruinekool <rbruinekool@gmail.com>
2018-02-16 16:25:00 +0100

    Mutebox server works, now debugging and making sure the mutebox can handle loss of network and internet

--- MuteBoxServer.py
+++ MuteBoxServer.py
@@ -40,6 +40,8 @@ while not(internetActive):
     except urllib2.URLError as err:
         print "No connection to internet, trying again in 5 seconds"
         time.sleep(5)
+        
+print([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
 
 muteBoxData = getMuteBoxData();
 
